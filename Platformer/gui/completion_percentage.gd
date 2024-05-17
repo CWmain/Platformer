ca4b7be67a88f06974a410fb72e291bd9b38dfd2
CWmain <rename1234@gmail.com>
2024-05-17 15:28:51 +1000

    Added a completion percentage to main screen to clearly show player progress

---
+++ Platformer/gui/completion_percentage.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var global = $"/root/Global"
+@onready var level_coin_count = global.level_coin_count
+
+@onready var percent = $Percent
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var value: float = calculate_percent() * 100
+	percent.text = "%.1f%%" % value
+	pass # Replace with function body.
+
+
+func calculate_percent() -> float:
+	var totalCoins: float = 0
+	var collectedCoins: float = 0
+	for level in level_coin_count:
+		totalCoins += level_coin_count[level]
+		collectedCoins += global.coins_collected(level)
+	return (collectedCoins/totalCoins)
+	
