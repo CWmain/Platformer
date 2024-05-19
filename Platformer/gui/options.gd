9e92f2424122d3614df11337073acd4251887161
CWmain <rename1234@gmail.com>
2024-05-19 14:34:41 +1000

    Added an are you sure button for delete save

--- Platformer/gui/options.gd
+++ Platformer/gui/options.gd
@@ -6,6 +6,8 @@ extends Control
 @onready var level_coin_count = global.level_coin_count
 @onready var tab_container = $TabContainer
 
+@onready var delete_double_check = $deleteDoubleCheck
+
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		print("escape pressed")
@@ -21,6 +23,10 @@ func _on_back_pressed():
 
 	
 func _on_delete_save_pressed():
+	delete_double_check.show()
+	
+
+func delete_save():
 	var save_path = "user://%s/%s.save"
 	for level in level_coin_count:
 		#No save then skip
@@ -33,4 +39,3 @@ func _on_delete_save_pressed():
 		dir.list_dir_end()
 	global.delete_continue()
 	Engine.time_scale = 1
-
